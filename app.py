42206836374d01f5b7ebf3054d2e7e5aa422ae24
mclbdn <michalbednar93@gmail.com>
2021-10-07 01:46:26 -0500

    Add placeholders

--- app.py
+++ app.py
@@ -24,10 +24,38 @@ def home():
 @app.route("/create", methods=["GET", "POST"])
 def create():
     form = TodoForm()
+    form.todo_input.render_kw = {"placeholder": "Create a new TODO"}
     if form.validate_on_submit():
         return redirect(url_for("index"))
 
-    return render_template("create.html", form=form)
+    return render_template("create.html", form=form, title="Create")
+
+@app.route("/read", methods=["GET", "POST"])
+def read():
+    form = TodoForm()
+    form.todo_input.render_kw = {"placeholder": "Search by text"}
+    if form.validate_on_submit():
+        return redirect(url_for("index"))
+
+    return render_template("read.html", form=form, title="Read")
+
+@app.route("/update", methods=["GET", "POST"])
+def update():
+    form = TodoForm()
+    form.todo_input.render_kw = {"placeholder": "Update by ID"}
+    if form.validate_on_submit():
+        return redirect(url_for("index"))
+
+    return render_template("update.html", form=form, title="Update")
+
+@app.route("/delete", methods=["GET", "POST"])
+def delete():
+    form = TodoForm()
+    form.todo_input.render_kw = {"placeholder": "Delete by ID"}
+    if form.validate_on_submit():
+        return redirect(url_for("index"))
+
+    return render_template("delete.html", form=form, title="Delete")
 
 if __name__ == "__main__":
     app.run(debug=True)
